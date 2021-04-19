5a0d601d76a2012065f9b107397a7c16e35a2005
Nuryslam <63663731+ndocx@users.noreply.github.com>
2021-04-19 10:56:29 +0600

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,6 @@
 	<title>SCA-19B webpage</title>
 </head>
 <body>
-This is SCA-19B students' first webpage.
+This is SCA-19B students' first webpage. Kadyr, Omur, Perizat, Suleiman.
 </body>
 </html>
